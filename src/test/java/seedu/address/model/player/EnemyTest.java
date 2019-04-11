cc4903b8476f9b1a9ba65ad9fdd07789e30b920c
lucydotc <lucy@u.nus.edu>
2019-04-11 23:06:17 +0800

    Add skeleton tests for all methods in Enemy

--- src/test/java/seedu/address/model/player/EnemyTest.java
+++ src/test/java/seedu/address/model/player/EnemyTest.java
@@ -41,6 +41,17 @@ public class EnemyTest {
         assertEquals(newPlayer1.getTargetHistory(), testPlayer.getTargetHistory());
     }
 
+
+    /**
+     * Setup:
+     *     Enemy has one ship vertically, on a1, with 1HP remaining
+     *     Enemy has another ship somewhere
+     *     Human attacks A1
+     * Expected result:
+     *     Human attack hits ship at A1
+     *     Ship HP decreases
+     *     a Hit AttackResult is returned
+     */
     @Test public void test_prepEnemy() {
         //stub
     }
@@ -113,7 +124,24 @@ public class EnemyTest {
         //stub
     }
 
+
+    /**
+     * Setup:
+     *     Watchlist is not empty
+     *     A status of DESTROYED was detected before this method call
+     * Expected result:
+     *     All elements in the watchlist are put in allPossibleTargets
+     *     and also in allParityTargets if it satisfied the hasParity() constraint
+     *     this method returns void
+     */
     @Test public void test_cleanseWatchlist() {
         //stub
+        //provide non empty watchlist
+        //copy this to watchlistCopy
+        //run cleanse watchlist
+        //check that watchlist is now empty
+        //check that all parity targets have parity
+        //allParityTargets is subset of allPossibleTargets
+        //allPossibleTargets should be super set of watchlistCopy
     }
 }
