e46d557bbcbcc94605916d3c79fc1b5eaf467983
lucydotc <lucy@u.nus.edu>
2019-04-12 20:55:12 +0800

    Add test for prepEnemy

--- src/test/java/seedu/address/model/player/EnemyTest.java
+++ src/test/java/seedu/address/model/player/EnemyTest.java
@@ -4,11 +4,17 @@ import static org.junit.Assert.assertEquals;
 import static org.hamcrest.CoreMatchers.is;
 import static org.assertj.core.api.Assertions.not;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static seedu.address.testutil.SizeTenMapGrid.initialisePlayerSizeTen;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 
+import seedu.address.model.cell.Coordinates;
 import seedu.address.model.cell.Status;
 
 public class EnemyTest {
@@ -24,6 +30,14 @@ public class EnemyTest {
         testPlayer = new Player("Bob", 5, 2, 1);
 
 
+    }
+
+    boolean isSubset(List<?> list, List<?> sublist) {
+        return Collections.indexOfSubList(list, sublist) != -1;
+    }
+
+    public static <T> boolean isSameListContents(List<T> list1, List<T> list2) {
+        return new HashSet<>(list1).equals(new HashSet<>(list2));
     }
 
     //Lucy TO-FIX:
@@ -65,17 +79,56 @@ public class EnemyTest {
 
 
     /**
+     * this method calls two other private methods:  fillWithAllCoords() and populateEnemyMap();
+     * testing will be done in two phases, one for  fillWithAllCoords() and one for populateEnemyMap();
+     *
      * Setup:
-     *     Enemy has one ship vertically, on a1, with 1HP remaining
-     *     Enemy has another ship somewhere
-     *     Human attacks A1
+     *     Enemy should first have:
+     *          empty lists: allPossibleTargets, allPossiblePopulateCoords, allParityTargets
+     *          empty deployed fleet
      * Expected result:
-     *     Human attack hits ship at A1
-     *     Ship HP decreases
-     *     a Hit AttackResult is returned
+     *     Enemy should have:
+     *          filled allPossibleTargets, allPossiblePopulateCoords, allParityTargets
+     *          allParityTargets should only contain coordinates that hasParity()
+     *          allParityTargets should be proper subset of allPossibleTargets
+     *          allPossiblePopulateCoords should be proper subset of allPossibleTargets
+     *          filled deployed fleet with the correct number of each type of battleships
+     *
      */
     @Test public void test_prepEnemy() {
-        //stub
+        //tests for fillWithAllCoords()
+
+        //first check empty lists: allPossibleTargets, allPossiblePopulateCoords, allParityTargets
+        assertTrue(testEnemy.getAllPossibleTargets().isEmpty());
+        assertTrue(testEnemy.getAllPossiblePopulateCoords().isEmpty());
+        assertTrue(testEnemy.getAllParityTargets().isEmpty());
+
+        //check that there are no deployed ships yet - deployedFleet is empty
+        assertTrue(testEnemy.getFleet().getDeployedFleet().isEmpty());
+
+        testEnemy.prepEnemy();
+
+        //check that allPossibleTargets, allPossiblePopulateCoords, allParityTargets are not empty
+        assertFalse(testEnemy.getAllPossibleTargets().isEmpty());
+        assertFalse(testEnemy.getAllPossiblePopulateCoords().isEmpty());
+        assertFalse(testEnemy.getAllParityTargets().isEmpty());
+
+        //check that allParityTargets should only contain coordinates that hasParity()
+        for (Coordinates parityElement : testEnemy.getAllParityTargets()) {
+            assertTrue(testEnemy.hasParity(parityElement.getRowIndex().getZeroBased(),
+                    parityElement.getColIndex().getZeroBased()));
+        }
+
+        //check that allParityTargets should be proper subset of allPossibleTargets
+        assertFalse(isSameListContents(testEnemy.getAllParityTargets(), testEnemy.getAllPossibleTargets()));
+        assertTrue(isSubset(testEnemy.getAllPossibleTargets(), testEnemy.getAllParityTargets()));
+
+        //check that allPossiblePopulateCoords should be proper subset of allPossibleTargets
+        assertFalse(isSameListContents(testEnemy.getAllPossiblePopulateCoords(), testEnemy.getAllPossibleTargets()));
+        assertTrue(isSubset(testEnemy.getAllPossiblePopulateCoords(), testEnemy.getAllParityTargets()));
+
+        //check that the correct number of ships were deployed into deployedFleet
+        assertTrue(testEnemy.getFleet().isAllDeployed());
     }
 
     @Test public void test_enemyShootAt() {
@@ -99,5 +152,6 @@ public class EnemyTest {
         assertThat(testEnemy.getLastAttackStatus(), is(not(Status.DESTROYED)));
 
     }
+
 
 }
